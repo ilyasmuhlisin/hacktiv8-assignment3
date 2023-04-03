1da4ac5c7af0a758f968882fbe046bc06da95214
ilyasmuhlisin <21ilyasmhlsn@gmail.com>
2023-04-03 15:42:11 +0700

    assignment3fix

--- src/items.js
+++
@@ -1,9 +0,0 @@
-let items = [
-  { id: 1, name: "God of blade", power: 1000 },
-  { id: 2, name: "Supreme dagger", power: 1200 },
-  { id: 3, name: "Hollow axes", power: 1300 },
-];
-
-export function getItems() {
-  return items;
-}
